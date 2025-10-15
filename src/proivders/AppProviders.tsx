8ac7c794a5a25b951051af77ddc171c3600ea18d
kimo871 <mk5391130@gmail.com>
2025-10-15 20:43:44 +0300

    feat : status bar content color to be visible

--- src/proivders/AppProviders.tsx
+++ src/proivders/AppProviders.tsx
@@ -1,11 +1,21 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../lib/client";
 import { ReactElement } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 
-export default function AppProviders({children}:{children:ReactElement | ReactElement[]}){
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    )
+export default function AppProviders({
+  children,
+}: {
+  children: ReactElement | ReactElement[];
+}) {
+  return (
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+         <StatusBar 
+            barStyle="dark-content" 
+          />
+        {children}</QueryClientProvider>
+    </SafeAreaProvider>
+  );
 }
